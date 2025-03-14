736562994f7fc3255c505bb150d3f6a095989e6a
joliveros0912 <116228315+joliveros0912@users.noreply.github.com>
2025-03-13 22:19:02 -0500

    Update settings.py

--- saray/settings.py
+++ saray/settings.py
@@ -117,13 +117,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'ingles/static')]
@@ -132,4 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Media files (Uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media' 
 ALLOWED_HOSTS = ['localhost','inglesss-production.up.railway.app']
