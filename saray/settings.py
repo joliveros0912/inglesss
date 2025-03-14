70d565eff898a69519c6669b7e002b8a1c5b9b73
joliveros0912 <116228315+joliveros0912@users.noreply.github.com>
2025-03-13 23:18:11 -0500

    Update settings.py

--- saray/settings.py
+++ saray/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'ingles/static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
